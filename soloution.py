bb68b85e10a5d35b1b69d247bb76a5c3e2a8096c
mohammad hossain <you@example.com>
2018-03-28 23:58:21 +0430

    initial commit

---
+++ soloution.py
@@ -0,0 +1,18 @@
+import socket
+
+ports=[21,22,25,80,110,9090]
+socket.setdefaulttimeout(5)
+f=open('host_names','r')
+host_names=f.read()
+host_names=host_names.split('\n')
+
+for x in host_names:
+    for y in ports:
+        try:
+            print(x+":{0}".format(y))
+            s=socket.socket()
+            s.connect((x,y))
+            ans=s.recv(1024)
+            print(ans)
+        except Exception as e:
+            print(e)
